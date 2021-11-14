f8d551f028995b0de37db60cb5c4ba03b1474143
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-11-14 15:32:29 +0530

    fix: videos deleted are also updated on backend

--- src/Routes/History/history.js
+++ src/Routes/History/history.js
@@ -3,10 +3,16 @@ import { Link } from 'react-router-dom';
 import {useStateContext,useThemeContext} from '../../contexts/index'; 
 import'./history.css'
 import '../likedVideos/likedVideos.css'
+import axios from "axios";
 export function History(){
 const{state:{history}}=useStateContext();
 const{theme}=useThemeContext();
-const{dispatch}=useStateContext()
+const{dispatch}=useStateContext();
+const removeFromHistory=async(dispatch,id)=>{
+    const response=await axios.delete(`https://video-library-server.vineetht.repl.co/history/${id}`)
+    console.log(response)
+    dispatch({type:'REMOVE_FROM_HISTORY',videoId:id})
+}
     return(
         <div className='playlist-videos'>
              <div className='heading'>
@@ -14,14 +20,13 @@ const{dispatch}=useStateContext()
                     <h3>History</h3>
             </div>
           {history.map((video)=>{
-              console.log(video)
           const{id}=video
                     return (
                             <div className='playlist-videos'>
                                 <Link to={`/videoPlayer/${id}`}>
                                     <VerticalVideoCard video={video}/>
                                 </Link>
-                                <button className='btn-primary' onClick={()=>dispatch({type:'REMOVE_FROM_HISTORY',videoId:id})}>
+                                <button className='btn-primary' onClick={()=>removeFromHistory(dispatch,id)}>
                                     <svg className={theme==='light'?'svg-img-black':'svg-img'} aria-hidden="true" role="img" width="1.5em" height="1.5em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M9 3v1H4v2h1v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6h1V4h-5V3H9M7 6h10v13H7V6m2 2v9h2V8H9m4 0v9h2V8h-2z"></path></svg>
                                 </button>
                                 <span className='toast-card'>Delete</span>
--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -3,5 +3,6 @@ import { useAuthContext } from "../../contexts/authContext";
 
 export function PrivateRoute({path, ...props }) {
   const {state:{ login} } = useAuthContext();
+  console.log('hello',login,path)
   return login ? <Route path={path} {...props} /> : <Navigate state={{from:path}} replace to="/login" />;
 }
