2345494f7d28955c9a737e191fae267260c9a825
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-12-23 10:56:17 +0530

    feat: added Regex for signUp form

--- src/Reducer/formErrorReducer.js
+++ src/Reducer/formErrorReducer.js
@@ -1,46 +1,44 @@
 
 export function formChecker(formState,errorDispatch){
-    const { fname, lname, emailId, password, confirmPassword } = formState;
-  let flag=true;
-  if(fname===''){
-    errorDispatch({type:'SET_FNAME_ERR',payload:'First Name Required'})
-    flag=false
-  } if(lname===''){
-    errorDispatch({type:'SET_LNAME_ERR',payload:'Last Name Required'})
-    flag=false
-  }
-  if(emailId===''){
-    errorDispatch({type:'SET_EMAIL_ERR',payload:'Email Id Name Required'})
-    flag=false;
-  } 
-   if(password===''){
-    errorDispatch({type:'SET_PASSWORD_ERR',payload:'Password Required'})
-    flag=false
-  }
-  if(confirmPassword!==password){
-    console.log(confirmPassword!==password)
-    errorDispatch({type:'SET_CONFIRM_PASSWORD_ERR',payload:'Password not Matching'})
-    flag=false
-  }
-  return flag;
+  const { fname, lname, emailId, password, confirmPassword } = formState;
+let flag=true;
+if(fname===''|| !/^[a-zA-Z]+(\s*\w*)*$/.test(fname)){
+  errorDispatch({type:'SET_FNAME_ERR',payload:'First Name Required'})
+  flag=false
+} if(lname===''|| !/^[a-zA-Z]+(\s*\w*)*$/.test(fname) ){
+  errorDispatch({type:'SET_LNAME_ERR',payload:'Last Name Required'})
+  flag=false
+}
+if(emailId===''|| !/^.+@.+\.com$/.test(emailId)){
+  errorDispatch({type:'SET_EMAIL_ERR',payload:'Email Id Name Required'})
+  flag=false;
+} 
+ if(password===''|| !/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g.test(password)){  
+  errorDispatch({type:'SET_PASSWORD_ERR',payload:'Alphanumeric(small,captial letters and length >8)'})
+  flag=false
+}
+if(confirmPassword!==password ){
+  errorDispatch({type:'SET_CONFIRM_PASSWORD_ERR',payload:'Password not Matching'})
+  flag=false
+}
+return flag;
 
 }
 export function errorHandler(state,{type,payload}){
-    switch(type){
-      case 'SET_FNAME_ERR':
-          return {...state,fname:payload}
-      case 'SET_LNAME_ERR':
-            return {...state,lname:payload}
-      case'SET_EMAIL_ERR':
-            return {...state,emailId:payload}
-      case'SET_PASSWORD_ERR':
-            return {...state,password:payload}
-      case 'SET_CONFIRM_PASSWORD_ERR':
-          console.log({payload})
-            return {...state,confirmPassword:payload}
-      case 'CLEARING_ERROR':
-            return {}
-      default:
-        return state
-    }
+  switch(type){
+    case 'SET_FNAME_ERR':
+        return {...state,fname:payload}
+    case 'SET_LNAME_ERR':
+          return {...state,lname:payload}
+    case'SET_EMAIL_ERR':
+          return {...state,emailId:payload}
+    case'SET_PASSWORD_ERR':
+          return {...state,password:payload}
+    case 'SET_CONFIRM_PASSWORD_ERR':
+          return {...state,confirmPassword:payload}
+    case 'CLEARING_ERROR':
+          return {}
+    default:
+      return state
   }
+}
