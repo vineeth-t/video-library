9e0ec565f1d5ce18427b688c6ce387dfce25ce23
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2021-12-23 10:55:54 +0530

    rename: changed dispatch to authDispatch

--- src/components/loginCard/login.jsx
+++ src/components/loginCard/login.jsx
@@ -1,38 +1,38 @@
 import { useLocation, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-import { useAuthContext } from '../../contexts/authContext';
-import { Profile } from '../../Routes';
+import { useStateContext,useAuthContext } from '../../contexts';
+import { loginHandler } from '../axios/axios.serverRequest';
 import './loginCard.css'
 export function Login(){
-    const{state:{login,userName,password},dispatch, loginInWithCredentials}=useAuthContext();
+    const{authState:{userName,password},authDispatch}=useAuthContext();
     const {state}=useLocation();
+    const{disptach}=useStateContext
     const navigate=useNavigate();
-
-    function loginHandler(event,state,userName,password,navigate){
-        event.preventDefault () 
-        loginInWithCredentials(state,userName,password,navigate);
-    }
     return(
         <>
-        {login?<Profile/>:
-            <form className='loginCard' onSubmit={(event)=>loginHandler(event,state,userName,password,navigate)}>
+            <form className='loginCard' onSubmit={(event)=>loginHandler(event,{state,userName:userName,password:password,authDispatch,navigate,disptach})}>
                 <div >
                     <label> UserName : </label>
-                    <input type='text' onChange={(event)=>dispatch({type:'SET-USER-NAME',payload:event.target.value})}/>
+                    <input type='text' onChange={(event)=>authDispatch({type:'SET-USER-NAME',payload:event.target.value})}/>
                 </div>
                 <div>
                     <label>Password : </label>
-                    <input type='password' onChange={(event)=>dispatch({type:'SET-PASSWORD',payload:event.target.value})}/>
+                    <input type='password' onChange={(event)=>authDispatch({type:'SET-PASSWORD',payload:event.target.value})}/>
                 </div>
                 <button className='btn-logIn'>LogIn</button>
+                <button className='btn-logIn' onClick={(event)=>{authDispatch({type:'SET-USER-NAME',payload:"admin@gmail.com"})
+                                                                          authDispatch({type:'SET-PASSWORD',payload:"admin@A1"})
+                                                                          loginHandler(event,{state,userName:'admin@gmail.com',password:'admin@A1',authDispatch,navigate})  
+            }}>LogIn With Test Credentials</button>
                 <div >
                 New user?
                 <Link to='/signUp'>
                      <span className='signUp'>SignUp</span>
                 </Link>
             </div>
+            
             </form>
-}
+
         </>
     )
 }
