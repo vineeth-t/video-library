6abafc0400e2caa2e158dca698f220c3f50bae9a
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-01-04 00:01:53 +0530

    added userName

--- src/components/navbar/leftNav/leftNav.js
+++ src/components/navbar/leftNav/leftNav.js
@@ -1,9 +1,10 @@
 import { NavLink } from 'react-router-dom';
-import { useThemeContext } from '../../../contexts';
+import { useThemeContext ,useAuthContext} from '../../../contexts';
 
 import './leftNav.css';
 export function LeftNav(){
   const{themeColor}=useThemeContext();
+  const{authState:{login,userName}}=useAuthContext()
     return (
         <ul style={themeColor}  className='leftNav' >
           <NavLink end to='/' >
@@ -34,6 +35,14 @@ export function LeftNav(){
           <li className="leftNav-menuList">
              <svg className="svg-image"  fill="currentcolor"  viewBox="0 0 1024 1024" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M536.1 273H488c-4.4 0-8 3.6-8 8v275.3c0 2.6 1.2 5 3.3 6.5l165.3 120.7c3.6 2.6 8.6 1.9 11.2-1.7l28.6-39c2.7-3.7 1.9-8.7-1.7-11.2L544.1 528.5V281c0-4.4-3.6-8-8-8zm219.8 75.2l156.8 38.3c5 1.2 9.9-2.6 9.9-7.7l.8-161.5c0-6.7-7.7-10.5-12.9-6.3L752.9 334.1a8 8 0 0 0 3 14.1zm167.7 301.1l-56.7-19.5a8 8 0 0 0-10.1 4.8c-1.9 5.1-3.9 10.1-6 15.1-17.8 42.1-43.3 80-75.9 112.5a353 353 0 0 1-112.5 75.9 352.18 352.18 0 0 1-137.7 27.8c-47.8 0-94.1-9.3-137.7-27.8a353 353 0 0 1-112.5-75.9c-32.5-32.5-58-70.4-75.9-112.5A353.44 353.44 0 0 1 171 512c0-47.8 9.3-94.2 27.8-137.8 17.8-42.1 43.3-80 75.9-112.5a353 353 0 0 1 112.5-75.9C430.6 167.3 477 158 524.8 158s94.1 9.3 137.7 27.8A353 353 0 0 1 775 261.7c10.2 10.3 19.8 21 28.6 32.3l59.8-46.8C784.7 146.6 662.2 81.9 524.6 82 285 82.1 92.6 276.7 95 516.4 97.4 751.9 288.9 942 524.8 942c185.5 0 343.5-117.6 403.7-282.3 1.5-4.2-.7-8.9-4.9-10.4z"></path></svg>
               <label className="item-name">History</label>
+          </li>
+          </NavLink>
+          <NavLink to={login?'/profile':'/login'}>
+          <li className="leftNav-menuList">
+          <svg className="svg-image" fill='currentcolor' width="2em" height="2em"  viewBox="0 0 24 24">
+                              <path d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2M7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.5.88 4.93 1.78A7.893 7.893 0 0 1 12 20c-1.86 0-3.57-.64-4.93-1.72m11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33A7.928 7.928 0 0 1 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.5-1.64 4.83M12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6m0 5a1.5 1.5 0 0 1-1.5-1.5A1.5 1.5 0 0 1 12 8a1.5 1.5 0 0 1 1.5 1.5A1.5 1.5 0 0 1 12 11z">
+                              </path></svg>
+              <label className="item-name">{userName}</label>
           </li>
           </NavLink>
       </ul>
