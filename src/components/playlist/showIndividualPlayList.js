6e0f0c0b880fa7c018a0b7e74ed2088c1b34945c
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2022-01-04 19:28:08 +0530

    Update showIndividualPlayList.js

--- src/components/playlist/showIndividualPlayList.js
+++ src/components/playlist/showIndividualPlayList.js
@@ -1,8 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
 import { useParams } from "react-router-dom"
+import { useEffect } from "react/cjs/react.development";
+import { useAuthContext, useStateContext, useThemeContext } from "../../contexts";
+import { API } from "../axios/axios.serverRequest";
 
 export function ShowIndividualPlayList(){
+    const{theme}=useThemeContext();
+    const{state:{playlists},dispatch}=useStateContext();
     const {playlistId}=useParams();
-    console.log(playlistId)
+    const[currentPlayList,setCurrentPlaylist]=useState(null)
+    const{authState:{userId}}=useAuthContext();
+    console.log(playlists,theme,currentPlayList)
+    useEffect(()=>{
+        (async function(){
+         const {data:{response,playlist,message}}= await axios.get(`${API}/playlists/${userId}/${playlistId}`)
+         console.log(playlist)
+         if(response){
+             setCurrentPlaylist(playlist)
+         }else{
+           dispatch({type:'TOAST',payload:message})
+         }
+         console.log('2')
+        }())
+     },[userId,playlistId,dispatch]);
     return (  
         <div>This is playlists</div>
     )}
